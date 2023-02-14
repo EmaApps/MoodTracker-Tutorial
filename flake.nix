e0bfde76cab69d0b31c1a3db8873adb7e2660992
Sridhar Ratnakumar <srid@srid.ca>
2023-02-13 21:33:55 -0500

    nix: Update inputs & deps (use Ema from nixpkgs)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
-  description = "Ema template app";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
 
-    # Haskell overrides
-    ema.url = "github:srid/ema/master";
-    ema.flake = false;
-    tailwind-haskell.url = "github:srid/tailwind-haskell";
-    tailwind-haskell.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         haskell-flake.flakeModule
@@ -21,37 +15,20 @@
       perSystem = { self', inputs', pkgs, ... }: {
         # "haskellProjects" comes from https://github.com/srid/haskell-flake
         haskellProjects.default = {
-          root = ./.;
-          haskellPackages = pkgs.haskell.packages.ghc924;
-          buildTools = hp: {
+          imports = [
+            inputs.nixpkgs-140774-workaround.haskellFlakeProjectModules.default
+          ];
+          devShell.tools = hp: {
             inherit (pkgs)
               treefmt
               nixpkgs-fmt
               foreman;
             inherit (hp)
-              # cabal-fmt
-              fourmolu;
-            inherit (inputs'.tailwind-haskell.packages)
-              tailwind;
-            # https://github.com/NixOS/nixpkgs/issues/140774 reoccurs in GHC 9.2
-            ghcid = pkgs.haskell.lib.overrideCabal hp.ghcid (drv: {
-              enableSeparateBinOutput = false;
-            });
-          };
-          source-overrides = {
-            inherit (inputs)
-              ema;
-          };
-          overrides = self: super: with pkgs.haskell.lib; {
-            inherit (inputs'.tailwind-haskell.packages)
-              tailwind;
-            relude = dontCheck (self.callHackage "relude" "1.1.0.0" { }); # 1.1 not in nixpkgs yet 
-            retry = dontCheck super.retry; # For GHC 9.2.
-            streaming-commons = dontCheck super.streaming-commons; # Fails on darwin
-            http2 = dontCheck super.http2; # Fails on darwin
-            type-errors-pretty = dontCheck (doJailbreak super.type-errors-pretty);
+              cabal-fmt
+              fourmolu tailwind;
           };
         };
+        packages.default = self'.packages.MoodTracker-Tutorial;
       };
     };
 }
